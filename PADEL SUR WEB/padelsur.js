d290653372666d097e156e64cb111fbff256480e
Facundo Calleja <facundocalleja@hotmail.com>
2022-02-21 20:12:36 -0300

    padelsurweb

--- PADEL SUR WEB/padelsur.js
+++ PADEL SUR WEB/padelsur.js
@@ -1,5 +1,22 @@
+$(window).scroll(function() {
+    if ($("#menugrande").offset().top < 100) {
+        $("#menugrande").addClass("navbarpadel");
+    } else {
+        $("#menugrande").removeClass("navbarpadel");
+     
+}
+}) 
+
+
+
+
+
 // selector
 var menu = document.querySelector('.hamburger');
+
+
+
+
 
 // method
 function toggleMenu (event) {
@@ -19,3 +36,50 @@ menu.addEventListener('click', toggleMenu, false);
 		return false;
 	});
 });*/
+
+
+// COMPARAR CONTRASEÑAS y ADMITIR ADMINISTRADOR
+
+//Defino el usuario y contraseña que se va a admitir
+
+
+
+//Comparar contraseñas y permitir ingreso
+let btn1 = document.querySelector('#btn1')
+//addEventListener -> escucha el evento del objeto seleccionado. (evento,funcion anonima a ejecutar -> callback)
+btn1.addEventListener("click",
+
+function(){
+    var usuario = 'Admin1';
+    var contrasenia = 221193;
+    let usuario1 = document.querySelector('#usuario').value
+    let contrasenia1 = document.querySelector('#contraseña').value
+    let resp = document.querySelector('#respuesta')
+        // or -> || (doble pipe)
+        // and -> &&
+        if(usuario1 === usuario){
+
+            if(contrasenia1 === contrasenia){
+                resp.innerHTML="Bienvenido al Panel de Administrador"
+                usuario1.style.background="green"
+                contrasenia1.style.background="green"
+            }else{
+                resp.innerHTML="El usuario o contraseña no coinciden. :("
+                usuario1.style.background="red"
+                contrasenia1.style.background="red"
+            }
+        }else{
+            
+            resp.innerHTML="Alguno de los datos esta vacío..."
+			}
+		
+		
+    }//cierra function()
+
+);
+
+
+//SCROLL BARRA NAV -//
+
+
+
